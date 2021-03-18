eec36e5d4078f713819ad222891e3d13798d712d
v.makarov <viollo190201@gmail.com>
2021-03-18 15:13:23 +0200

    Add project files.

---
+++ TeamLead.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salary
+{
+    class TeamLead
+    {
+        public double Salary { get; private set; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int ID { get; private set; }
+
+        List<Employer> EmpLead = new List<Employer>();
+
+        public TeamLead
+    }
+}
